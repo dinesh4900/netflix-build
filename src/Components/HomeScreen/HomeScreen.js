6a901b771e2457ed7c5a379c485696c5690904ff
dineshking <dineshm4900@gmail.com>
2021-02-04 14:16:21 +0530

    added header

---
+++ src/Components/HomeScreen/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+import './HomeScreen.css';
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            <NavBar />
+           
+
+            {/* banner */}
+            {/* rows */}
+        </div>
+    )
+}
+
+export default HomeScreen
