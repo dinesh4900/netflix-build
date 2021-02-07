06c5d0e784f106705792420824feb0cbeaa79e85
dineshking <dineshm4900@gmail.com>
2021-02-07 15:52:28 +0530

    completed frontend and API

--- src/Components/HomeScreen/HomeScreen.js
+++ src/Components/HomeScreen/HomeScreen.js
@@ -16,9 +16,9 @@ function HomeScreen() {
             <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
             <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
             <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-            <Row title="Horror Movies" fetchUrl={requests.fetchComedyMovies} />
-            <Row title="Romance Movies" fetchUrl={requests.fetchComedyMovies} />
-            <Row title="Documentaries" fetchUrl={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
         </div>
     )
 }
--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -1,9 +1,12 @@
-import axios from '../../axios';
+
 import React, { useEffect, useState } from 'react';
 import './Row.css';
+import axios from '../../axios';
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
     const [movies, setMovies] = useState([]);
+
+    const base_url = "https://image.tmdb.org/t/p/original/";
 
     
 
@@ -23,9 +26,28 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
         <div className="row">
             <h2>{title}</h2>
 
+            <div className="row__posters">
+                {movies.map(
+                    (movie) => 
+                    ((isLargeRow && movie.poster_path) ||
+                        (!isLargeRow && movie.backdrop_path)) && ( 
+                        <img 
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+                            key={movie.id}
+                            
+                            src={`${base_url}${
+                            isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            alt={movie.name} 
+                        />
+                    )
+                )}
+            </div>
+
+           
+
             
         </div>
-    )
+    );
 }
 
 export default Row
